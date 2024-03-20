f2ed55b7d003d99416b516267d0e8047ddc2a63b
Vildan <vildan@awkwardstyles.com>
2024-03-20 10:34:14 +0300

    Add modal to movie items

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MovieList = (props) => {
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
+
   return (
     <div className="movie-list container-fluid">
       <div className="row justify-content-center">
@@ -8,6 +18,7 @@ const MovieList = (props) => {
           <div
             className="col-xs-6 mt-3 movie-list__item col-md-4 col-lg-3"
             key={movie.imdbID}
+            onClick={() => handleMovieClick(movie)}
           >
             <img src={movie.Poster} alt="movie"></img>
             <p>{movie.Title}</p>
@@ -15,6 +26,36 @@ const MovieList = (props) => {
           </div>
         ))}
       </div>
+
+      {selectedMovie && (
+        <div className="modal-container">
+          <div
+            className="modal"
+            style={{ display: "block" }}
+            onClick={handleCloseModal}
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">{selectedMovie.Title}</h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    aria-label="Close"
+                    onClick={handleCloseModal}
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <img src={selectedMovie.Poster} alt="movie"></img>
+                  <p>{selectedMovie.Year}</p>
+                  <p>{selectedMovie.Plot}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="modal-backdrop show" onClick={handleCloseModal}></div>
+        </div>
+      )}
     </div>
   );
 };
